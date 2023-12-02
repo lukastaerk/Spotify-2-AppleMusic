5aeacce17a1b3ce9c71e388be99cdceac58deef9
Luka Stärk <luka.staerk@outlook.com>
2023-12-02 14:17:45 +0100

    now possible to add albums from the spotify export and fixed checking for playlist existance before creation

--- convertsongs.py
+++ convertsongs.py
@@ -1,3 +1,4 @@
+import argparse
 from sys import argv
 import csv
 import urllib.parse, urllib.request
@@ -6,6 +7,8 @@ from time import sleep
 import requests
 import os
 
+SLEEPING_SEC = 1
+BASE_URL_AM = "https://amp-api.music.apple.com/v1"
 # Checking if the command is correct
 if len(argv) > 1 and argv[1]:
     pass
@@ -21,24 +24,44 @@ def get_connection_data(f,prompt):
     else:
             return input(prompt)
 
-def create_apple_music_playlist(session, playlist_name):
-    url = "https://amp-api.music.apple.com/v1/me/library/playlists"
+def get_all_my_playlists(session):
+    url = f"{BASE_URL_AM}/me/library/playlists"
+    all_playlists = {}
+    limit = 100  # Adjust as per API's allowed maximum
+    offset = 0
+
+    while True:
+        params = {'limit': limit, 'offset': offset}
+        response = session.get(url, params=params)
+        if response.status_code != 200:
+            break
+
+        data = response.json()
+        playlists = data.get('data', [])
+        all_playlists.update([(pl['attributes']['name'], pl['id']) for pl in playlists])
+
+        if len(playlists) < limit:
+            break  # No more playlists to fetch
+        offset += limit
+
+    return all_playlists
+
+def create_apple_music_playlist(session, playlist_name, all_my_playlists, description="A new playlist created via API"):
+    url = f"{BASE_URL_AM}/me/library/playlists"
     data = {
         'attributes': {
             'name': playlist_name,
-            'description': 'A new playlist created via API'
+            'description': description,
         }
     }
-    # test if playlist exists and create it if not
-    response = session.get(url)
-    if response.status_code == 200:
-        for playlist in response.json()['data']:
-            if playlist['attributes']['name'] == playlist_name:
-                print(f"Playlist {playlist_name} already exists!")
-                return playlist['id']
+    
+    if playlist_name in all_my_playlists:
+        print(f"Playlist {playlist_name} already exists!")
+        return all_my_playlists[playlist_name]
+    
     response = session.post(url, json=data)
     if response.status_code == 201:
-        sleep(1.5)
+        sleep(SLEEPING_SEC)
         return response.json()['data'][0]['id']
     else:
         raise Exception(f"Error {response.status_code} while creating playlist {playlist_name}!")
@@ -126,7 +149,7 @@ def get_itunes_id(title, artist, album):
 # Function to add a song to a playlist
 def add_song_to_playlist(session, song_id, playlist_id, playlist_name):
     try:   
-        request = session.post(f"https://amp-api.music.apple.com/v1/me/library/playlists/{playlist_id}/tracks", json={"data":[{"id":f"{song_id}","type":"songs"}]})
+        request = session.post(f"{BASE_URL_AM}/me/library/playlists/{playlist_id}/tracks", json={"data":[{"id":f"{song_id}","type":"songs"}]})
         # Checking if the request is successful
         if requests.codes.ok:
             print(f"Song {song_id} added to playlist {playlist_name}!")
@@ -142,7 +165,7 @@ def add_song_to_playlist(session, song_id, playlist_id, playlist_name):
 def get_playlist_track_ids(session, playlist_id):
     # test if song is already in playlist
     try:
-        response = session.get(f"https://amp-api.music.apple.com/v1/me/library/playlists/{playlist_id}/tracks")
+        response = session.get(f"{BASE_URL_AM}/me/library/playlists/{playlist_id}/tracks")
         if response.status_code == 200:
             #print(response.json()['data'])
             return [track['attributes']['playParams']['catalogId'] for track in response.json()['data']]
@@ -155,30 +178,16 @@ def get_playlist_track_ids(session, playlist_id):
         raise Exception(f"Error while getting playlist {playlist_id}!")
         return None
 # Opening session
-def create_playlist_and_add_song(file):
-    with requests.Session() as s:
-        s.headers.update({"Authorization": f"{token}",
-                    "media-user-token": f"{media_user_token}",
-                    "Cookie": f"{cookies}",
-                    "Host": "amp-api.music.apple.com",
-                    "Accept-Encoding":"gzip, deflate, br",
-                    "Referer": "https://music.apple.com/",
-                    "Origin": "https://music.apple.com",
-                    "Content-Length": "45",
-                    "Connection": "keep-alive",
-                    "Sec-Fetch-Dest": "empty",
-                    "Sec-Fetch-Mode": "cors",
-                    "Sec-Fetch-Site": "same-site",
-                    "TE": "trailers"})
+def create_playlist_and_add_song(session, file, all_my_playlists):
     
     # Getting the playlist name
     playlist_name = os.path.basename(file)
     playlist_name = playlist_name.split('.')
     playlist_name = playlist_name[0]
 
-    playlist_identifier = create_apple_music_playlist(s, playlist_name)
+    playlist_identifier = create_apple_music_playlist(session, playlist_name, all_my_playlists)
 
-    playlist_track_ids = get_playlist_track_ids(s, playlist_identifier)
+    playlist_track_ids = get_playlist_track_ids(session, playlist_identifier)
     print(playlist_track_ids)
     # Opening the inputed CSV file
     with open(str(file), encoding='utf-8') as file:
@@ -203,7 +212,6 @@ def create_playlist_and_add_song(file):
                     failed += 1
                     continue
                 print(f'\nN°{n} | {title} | {artist} | {album} => {track_id}')
-                sleep(0.5)
                 if add_song_to_playlist(s, track_id, playlist_identifier, playlist_name):
                     converted += 1
                 else:
@@ -211,26 +219,89 @@ def create_playlist_and_add_song(file):
             # If not, write it in a file
             else:
                 print(f'N°{n} | {title} | {artist} | {album} => NOT FOUND')
-                with open(f'{playlist_name}_noresult.txt', 'a+', encoding='utf-8') as f:
-                    f.write(f'{title} | {artist} | {album} => NOT FOUND')
-                    f.write('\n')
+                with open(f'noresult.csv', 'a+', encoding='utf-8') as f:
+                    f.write(f'{playlist_name};{title};{artist};{album}; NOT FOUND\n')
                 failed += 1
-            sleep(1.5)
+            sleep(SLEEPING_SEC)
     # Printing the stats report
     print(f'\n - STAT REPORT -\nPlaylist Songs: {n}\nConverted Songs: {converted}\nFailed Songs: {failed}\nPlaylist converted at {round(converted/n*100)}%')
 
+def add_albums_to_library(session, albums):
+    for item in albums:
+        album = item['album']
+        artist = item['artist']
+        album_id = get_album_id(session, album, artist)
+        if album_id:
+            ans = session.post(f"{BASE_URL_AM}/me/library?ids[albums]={album_id}", json=[])  
+            if ans.status_code == 202:
+                print(f"Album {album} by {artist} added to library!")
+            else:
+                print(f"Error {ans.status_code} while adding album {album} by {artist} to library!")
+        else:
+            print(f"Album {album} by {artist} not found!")
+            with open(f'albums_noresult.csv', 'a+', encoding='utf-8') as f:
+                f.write(f'{album};{artist}; NOT FOUND\n')
+            continue
+        
+def get_album_id(session, album_name, artist_name):
+    storefront = "us"  # change to the appropriate country or region code
+    base_url = f"{BASE_URL_AM}/catalog/{storefront}/search"
+    query = {
+        'term': f'{album_name} {artist_name}',
+        'types': 'albums',
+        'limit': 5  # assuming you want the first match
+    }
+    response = session.get(base_url, params=query)
+    if response.status_code == 200:
+        results = response.json().get('results', {}).get('albums', {}).get('data', [])
+        if results:
+            return results[0]['id']  # returns the ID of the first album found
+        else:
+            return None
+    else:
+        print(f"Error {response.status_code} while getting album {album_name} by {artist_name}!")
+        return None
+    
+def argument_parsing():
+    parser = argparse.ArgumentParser(description='Convert a Spotify playlist, your liked albums or artists to Apple Music.')
+    parser.add_argument('-p', '--playlist', type=str, help='playlist csv-file or directory containing playlist csv-files')
+    parser.add_argument('-a', '--albums', type=str, help='YourLibrary.json from your Spotify data')
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    if len(argv) > 1 and argv[1]:
-        if ".csv" in argv[1]:
-            create_playlist_and_add_song(argv[1])
+    args = argument_parsing()
+    with requests.Session() as s:
+        s.headers.update({"Authorization": f"{token}",
+                    "media-user-token": f"{media_user_token}",
+                    "Cookie": f"{cookies}",
+                    "Host": "amp-api.music.apple.com",
+                    "Accept-Encoding":"gzip, deflate, br",
+                    "Referer": "https://music.apple.com/",
+                    "Origin": "https://music.apple.com",
+                    "Content-Length": "45",
+                    "Connection": "keep-alive",
+                    "Sec-Fetch-Dest": "empty",
+                    "Sec-Fetch-Mode": "cors",
+                    "Sec-Fetch-Site": "same-site",
+                    "TE": "trailers"})
+        
+    if args.playlist:
+        all_my_playlists = get_all_my_playlists(s)
+        if ".csv" in args.playlist:
+            create_playlist_and_add_song(s, args.playlist, all_my_playlists)
         else:
             # get all csv files in the directory argv[1]
-            files = [f for f in os.listdir(argv[1]) if os.path.isfile(os.path.join(argv[1], f))]
+            files = [f for f in os.listdir(args.playlist) if os.path.isfile(os.path.join(args.playlist, f))]
             # loop through all csv files
             for file in files:
                 if ".csv" in file:
-                    create_playlist_and_add_song(os.path.join(argv[1], file))
+                    create_playlist_and_add_song(s, os.path.join(args.playlist, file), all_my_playlists)
+    if args.albums:
+        json_file = open(args.albums, encoding='utf-8')
+        json_data = json.load(json_file)
+        json_file.close()
+        albums = json_data['albums']
+        add_albums_to_library(s, albums)
 
 # Developped by @therealmarius on GitHub
 # Based on the work of @simonschellaert on GitHub
